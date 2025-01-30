76b5a671f8700ca2a03510358cbfad10aab7a681
Michelle Lee <michellelee@Michelles-MBP.lan>
2025-01-30 11:25:57 -0800

    adding basic search

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,8 @@ export const API_BASE_URL = "https://frontend-take-home-service.fetch.com";
 export const API_LOGIN_ENDPOINT = "https://frontend-take-home-service.fetch.com/auth/login";
 export const API_LOGOUT_ENDPOINT = "https://frontend-take-home-service.fetch.com/auth/logout";
 export const API_DOG_BREEDS = "https://frontend-take-home-service.fetch.com/dogs/breeds";
-export const API_DOG_SEARCH = "https://frontend-take-home-service.fetch.com/dogs/search"
+export const API_DOG_SEARCH = "https://frontend-take-home-service.fetch.com/dogs/search";
+export const API_DOGS = "https://frontend-take-home-service.fetch.com/dogs"
+
+// Auth cookie
+export const AUTH_COOKIE_NAME = "fetch-access-token";
--- src/index.js
+++ src/index.js
@@ -2,24 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router";
-import reportWebVitals from './reportWebVitals';
 import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/" element={<LoginPage/>}/>
         <Route path="/home" element={<HomePage/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
